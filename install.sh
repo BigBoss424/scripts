4722bcbf0db81769ec22a51e1a9e445e683856bb
BigBoss424 <ajones061392@gmail.com>
2014-05-15 11:33:11 -0700

    Added my custom linux bash script for local installations

---
+++ install.sh
@@ -0,0 +1,12 @@
+#Installing software
+sudo apt-get install guake terminator compizconfig-settings-manager chromium-browser
+cinnamon bluefish git  -y
+
+#Updating software
+sudo apt-get update -y
+
+#Upgrading software
+sudo apt-get upgrade -y 
+
+#Rebooting Computer 
+sudo reboot
