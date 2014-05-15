799f725644a1fe34050f724fbb560525764337c2
BigBoss424 <ajones061392@gmail.com>
2014-05-15 14:17:46 -0700

    Added Steam repository, spotify repository, steam key, spotify key, as well as added them to the install part of the script. Also added evolution and thunderbird to the install script as well.

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #Installing software
 sudo apt-get install guake terminator compizconfig-settings-manager chromium-browser
-cinnamon bluefish git wine netflix-desktop steam -y
+cinnamon bluefish git wine netflix-desktop steam-launcher spotify-client -y
 
 
 #Adding Repositories 
 sudo add-apt-repository ppa:pipelight/stable
 #sudo deb http://repo.steampowered.com/steam/precise steam
+sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+sudo add-apt-repository "deb http://repo.steampowered.com/steam/ precise steam"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+wget -O- http://repo.steampowered.com/steam/signature.gpg | sudo apt-key add -
 
 #Updating software
 sudo apt-get update -y
@@ -15,4 +19,6 @@ sudo apt-get upgrade -y
 
 #Rebooting Computer 
 sudo reboot
+
+
 
