98e7cce822515f106ab04efd41cc9d289b9a87ee
BigBoss424 <ajones061392@gmail.com>
2014-05-19 17:00:54 -0700

    Added codeblocks, mono (C# compiler), mingw-w64 (C++ Compiler crossplatform with 32bit and 64bit processors).

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ cinnamon bluefish git wine netflix-desktop steam-launcher spotify-client compiz-
 compiz-core compiz-plugins-extra compiz-plugins-main filezilla gedit autokey-qt autokey-common 
 autokey-gtk clementine deluge terminator virtualbox-4.3 pidgin pidgin-encryption pidgin-themes 
 pidgin-twitter bluefish anjuta glade gcc-4.8 kdevelop inkscape imagemagick gcolor2 phpmyadmin
-drush tasksel lynx ruby-full rubygems1.8 gnustep monodevelop -y
+drush tasksel lynx ruby-full rubygems1.8 gnustep monodevelop mono-4.0-gac mono-4.0-service 
+mono-addins-utils codeblocks mingw-w64  -y
 
 #Installing Ruby gems
 sudo gem install sass compass zen-grids sassy-buttons -y 
