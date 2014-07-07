f5cbf6294ef7070d298630804840d3675ef68f56
BigBoss424 <ajones061392@gmail.com>
2014-07-07 14:57:56 -0700

    Added retext to the install script including that reposiotry apt-get.

--- install.sh
+++ install.sh
@@ -12,12 +12,13 @@ wordpress-openid wordpress-shibboleth wordpress-xrds-simple openssh-client opens
 openssh-server openssh-blacklist-extra php5-gd libssh2-php tlp tlp-rdw rar php-apc php5-intl vsftpd 
 scanmem netbeans refdb-clients refdb-doc refdb-server refdb netstat-nat iptraf nagios3 cacti ksysguard
 vpnc desmume visualboyadvance-gtk audacity openshot cinelerra-cv recordmydesktop gtk-recordmydesktop 
-vokoscreen fbreader -y
+vokoscreen fbreader retext -y
 
 #Installing Ruby gems
 sudo gem install sass compass zen-grids sassy-buttons -y 
 
 #Adding Repositories
+sudo add-apt-repository ppa:mitya57/retext-beta
 sudo add-apt-repository ppa:vokoscreen-dev/vokoscreen
 sudo apt-add-repository ppa:cinelerra-ppa/ppa 
 sudo add-apt-repository ppa:linrunner/tlp
