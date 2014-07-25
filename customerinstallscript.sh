6cbca2a4e6b7ae1c56abad61e836634b6835192e
BigBoss424 <ajones061392@gmail.com>
2014-07-25 14:10:17 -0700

    Added a new file for the customer installs of linux mint

---
+++ customerinstallscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+
+#Installing software
+sudo apt-get install seamonkey inkscape gimp openssh-server openssh-client
+netstat unetbootin -y
+
+#Updating Software
+sudo apt-get update -y 
+
+#Upgradig software 
+sudo apt-get upgrade -y
+#Rebooting Computer
+sudo reboot
