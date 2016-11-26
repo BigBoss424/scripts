e45359d64dc239ab8a44ab32e40891ed9b4f14fd
Aaron Jones <ajones061392@gmail.com>
2016-11-26 00:47:11 -0800

    Added these scripts to help me better manage my git repositories on github

---
+++ gitCloneAll.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+#Cloning the entire account of github
+
+echo "Cloning repository: northidahostar....\n"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/northidahostar
+echo "Cloning repository: JSS...."
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/JSS
+echo "Cloning repository: CandUnix...."
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/CandUnix
+echo "Cloning repository: Daily Programmer"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/Dailyprogrammer
+echo "Cloning repository: SansCMS"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/SansCMS
+echo "Cloning repository: ninjaswithguns"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/ninjaswithguns
+echo "Cloning repository: 4studentsbystudents"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/4studentsbystudents
+echo "Cloning repository: ZonamaDev"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/ZonamaDev
+echo "Cloning repository: nand2tetris"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/nand2tetris
+echo "Cloning repository: webtutorials"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/webtutorials
+echo "Cloning repository: FacebookClone"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/FacebookClone
+echo "Cloning repository: scripts"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/scripts
+echo "Cloning repository: redditbotpy"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/redditbotpy
+echo "Cloning repository: linuxkernelstuff"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/linuxkernelstuff
+echo "Cloning repository: phpstuff"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/phpstuff
+echo "Cloning repository: swift-tutorials"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/swift-tutorials
+echo "Cloning repository: OCAStudy"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/OCAStudy
+echo "Cloning repository: portfolio"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/portfolio
+echo "Cloning repository: ImageSite"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/ImageSite
+echo "Cloning repository: SocialNetworkPHP"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/SocialNetworkPHP
+echo "Cloning repository: c-c--"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/c-c--
+echo "========================================\n"
+echo "Cloning repository: a-zplumbing"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/a-zplumbing
+echo "========================================\n"
+echo "Cloning repository: foxNewsFilter"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/foxNewsFilter
+echo "========================================\n"
+echo "Cloning repository: KardashianFilter"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/KardashianFilter
+echo "========================================\n"
+echo "Cloning repository: DonaldTrumpFilter"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/DonaldTrumpFilter
+echo "Cloning repository: eqpublishing"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/eqpublishing
+echo "========================================\n"
+echo "Cloning repository: DataStructures"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/DataStructures
+echo "========================================\n"
+echo "Cloning repository: CSCD211"
+echo "========================================\n"
+git clone https://www.github.com/bigboss424/CSCD211
---
+++ gitclone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Cloning git repositories 
+
+echo "CLoning nand2tetris...."
+git clone https://www.github.com/bigboss424/nand2tetris
+echo "Cloning phpstuff...."
+git clone https://www.github.com/bigboss424/phpstuff
+echo "Cloning portfolio...."
+git clone https://www.github.com/bigboss424/portfolio
