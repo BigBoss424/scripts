bc6a172b921170a57892089997a027191f34fe12
Aaron Jones <ajones061392@gmail.com>
2017-02-09 19:05:29 -0800

    Working on a script to download series of movies, clips, and music from youtube.

---
+++ youtube-dl.sh
@@ -0,0 +1,8 @@
+
+#	This script will serve as a template to extract youtube videos 
+#	From youtube to be able to extract a large sum of videos from a 
+#	text file by extracting the youtube video urls and running them 
+#	through this program. 
+
+#!/bin/bash
+
